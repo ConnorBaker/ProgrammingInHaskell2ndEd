2b638f0fb9843989f11aeb84ecedbd832fde4ae4
Connor Baker <connorbaker01@gmail.com>
2019-02-19 13:08:08 -0500

    [Chapter6] Solutions 1 - 6b

--- Chapter6/app/Main.hs
+++ Chapter6/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Solutions
 
 main :: IO ()
-main = someFunc
+main = helloWorld
---
+++ Chapter6/src/Solutions.hs
@@ -0,0 +1,59 @@
+module Solutions
+    ( helloWorld
+    , facRecursive
+    , sumdown
+    , (^)
+    , euclid
+    , and
+    , concat
+    ) where
+
+-- Allows us to avoid a namespace conflict with our own implementation
+import Prelude hiding
+    ( (^)
+    , and
+    , concat
+    )
+
+-- #1
+facRecursive :: Int -> Int
+facRecursive n
+    | n < 0     = 0
+    | n == 0    = 1
+    | otherwise = n * facRecursive (n - 1)
+
+-- #2
+sumdown :: Int -> Int
+sumdown 0 = 0
+sumdown n = n + sumdown (n - 1)
+
+-- #3
+-- Assumes that the answer is computable; no 0^0
+-- We use "^^" to avoid a name conflict with Prelude
+(^) :: Int -> Int -> Int
+0 ^ _ = 0
+_ ^ 0 = 1
+n ^ m = n * (n ^ (m - 1))
+
+-- #4
+euclid :: Int -> Int -> Int
+euclid n m 
+    | n == m = n
+    | n >  m = euclid m (n - m)
+    | n <  m = euclid n (m - n)
+
+-- #6a
+and :: [Bool] -> Bool
+and []  = False
+and [x] = x
+and (x:xs)
+    | x == False = False
+    | x == True  = and xs
+
+-- #6b
+concat :: [[a]] -> [a]
+concat  []  = []
+concat (xs:xss) = xs ++ concat xss
+
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello world"
